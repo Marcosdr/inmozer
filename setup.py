7b208079f5c00d06ce55fe32651fefcea1116c72
Alejandro del Real <alejandro.delreal@playspace.com>
2022-06-29 16:47:19 +0200

    Starting proyect

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Inmozerg',
+      version='1.0',
+      description='Inmozerg project',
+      author='Marcos Duran y Alejandro del REal',
+      author_email='info@infozerg.com',
+      url='https://www.inmozerg.com/',
+      packages=['distutils', 'distutils.command'],
+      )
